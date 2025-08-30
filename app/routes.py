f6ec663fc864d9d26a9c261a17bc8b39cb618d12
Guptahariom28 <hariomgupta2809@gmail.com>
2025-08-30 13:55:27 +0530

    first commit

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def hrllo():
+    user= {'username': 'Hariom'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
