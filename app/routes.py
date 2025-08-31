1f6d5c60614d180e2af4a86be434a2e25dae8879
Guptahariom28 <hariomgupta2809@gmail.com>
2025-09-01 00:47:19 +0530

    aded sign in form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
+@app.route('/index')
 def hrllo():
     user= {'username': 'Hariom'}
     posts = [
@@ -15,3 +17,13 @@ def hrllo():
         }
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form= LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
